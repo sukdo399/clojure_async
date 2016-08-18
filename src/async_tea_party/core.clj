18c915fa4f893b3313a2fd4ef4344b2141f3a01a
Seokha Ko <sh.ko@lge.com>
2016-08-18 06:45:01 -0400

    async test.

--- src/async_tea_party/core.clj
+++ src/async_tea_party/core.clj
@@ -1,6 +1,45 @@
-(ns async-tea-party.core)
+(ns async-tea-party.core
+  (:require [clojure.core.async
+             :refer [>! <! >!! <!! chan close! go go-loop alts!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;(let [tea-channel (chan)]
+;  (go (>! tea-channel :cup-of-tea1))
+;  (go (println "Thanks for the" (<! tea-channel))))
+;
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(def tea-channel (chan 10))
+;
+;
+;(go-loop []
+;  (println "Thanks for the" (<! tea-channel))
+;  (recur))
+;
+;(>!! tea-channel :hot-cup-of-tea)
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(def tea-channel (chan 10))
+;(def milk-channel (chan 10))
+;(def sugar-channel (chan 10))
+;
+;(go-loop []
+;  (let [[v ch] (alts! [tea-channel
+;                       milk-channel
+;                       sugar-channel])]
+;    (println "Got" v "from" ch)
+;    (recur)))
+;
+;(>!! sugar-channel :sugar)
+;(>!! milk-channel :milk)
+;(>!! tea-channel :tea)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
